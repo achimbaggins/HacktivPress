1975cf66f34fcd48318303e91955e0b596d5f192
achimbaggins <achim_baggins@yahoo.com>
2017-10-12 16:33:59 +0700

    form tambah artikel

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -16,23 +16,11 @@
           <li><router-link to="/category/berita">Berita</router-link></li>
           <li><router-link to="/category/tutorial">Tutorial</router-link></li>
           <li><router-link to="/category/teknologi">Teknologi</router-link></li>
-          <!-- <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-              <li class="divider"></li>
-              <li><a href="#">One more separated link</a></li>
-            </ul>
-          </li> -->
+
         </ul>
 
         <ul class="nav navbar-nav navbar-right">
           <li><router-link to="/dashboard">LOGIN</router-link></li>
-          <li @click="doLogout"><router-link to="">LOGOUT</router-link></li>
         </ul>
       </div>
     </div>
--- client/src/components/UserHome.vue
+++ client/src/components/UserHome.vue
@@ -1,6 +1,43 @@
 <template lang="html">
   <div class="">
-    User Home
+    <h1>User Home</h1>
+    <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myNewArticle">Add New Article</button>
+    <button type="button" class="btn btn-danger" @click="doLogout">Logout</button>
+    <ul class="list-group text-left" >
+      <li class="list-group-item" v-for="post in userPost">
+        <span class="badge">Delete</span>
+        <span class="badge">Edit</span>
+        <b>{{post.title}}</b>
+      </li>
+    </ul>
+
+    <!-- Modal New Article -->
+    <div class="modal fade text-left" id="myNewArticle" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">New Article Form</h4>
+          </div>
+          <div class="modal-body">
+              <form>
+                <label>Judul</label>
+                <input type="text" class="form-control">
+                <label>Konten</label>
+                <textarea name="name" rows="8" cols="80" class="form-control"></textarea>
+                <label>Kategori</label>
+                <select class="" name="" class="form-control">
+                  <option value="berita">Berita</option>
+                  <option value="tutorial">Tutorial</option>
+                  <option value="teknologi">Teknologi</option>
+                </select>
+                <button type="submit" name="button" class="btn btn-primary">Kirim Artikel</button>
+              </form>
+        </div>
+      </div>
+    </div>
+  </div>
+    <!--  Modal New Article-->
   </div>
 </template>
 
@@ -18,6 +55,10 @@ export default {
       .then(({data}) => {
         this.userPost = data
       })
+    },
+    doLogout () {
+      localStorage.clear()
+      this.$router.push('/')
     }
   },
   created () {
